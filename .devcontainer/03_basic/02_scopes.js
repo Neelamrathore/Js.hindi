b57aafd9241586d60c12e1e0befabb5045aac223
Neelam Kanwar <104199338+Neelamrathore@users.noreply.github.com>
2026-05-12 11:46:37 +0000

    scopes.js

--- .devcontainer/03_basic/02_scopes.js
+++ .devcontainer/03_basic/02_scopes.js
@@ -2,8 +2,53 @@ let a =300
 if (true){
     let a = 10
     const b =20
-    console.log("inner: " + a); // 10
+    //console.log("inner: " + a); // 10
 }
 
-console.log(a);// 300
+//console.log(a);// 300
+
+
+function one() {
+    const username = "John";
+
+    function two() {
+        const website = "youtube";
+        //console.log(username); // John
+
+
+
+
+}
+        
+        //console.log(website); // youtube
+    two()
+}
+
+one()
+
+
+
+if (true) {
+    const username = "John";
+    if (username === "John")    {
+        const website = " youtube"
+        //console.log(username+website); // John youtube  
+
+
+
+    }    //console.log(website); // youtube
+
+
+}
+
+//++++++++++++++++intersting+++++++++++
+
+function addone(num) {
+    return num + 1
+}
+
+const addTwo = function(num) {
+    return num + 2
+}
+addTwo(5) // 7
 
